3750a174151cd7bb6e55d4e305adabcd4fbadf61
Ubuntu <ubuntu@ip-172-31-6-250.ap-northeast-2.compute.internal>
2021-12-05 06:55:20 +0000

    link 수정

--- myapp/views/result.js
+++ myapp/views/result.js
@@ -23,7 +23,7 @@ module.exports = {
           <br>
           <input type="button" onclick="sendLinkCustom();" value="공유하기"/>
           <h3></h3>
-          <input type='button' value='전체결과 보기' onclick="location.href='.'">
+          <input type='button' value='전체결과 보기' onclick="location.href='../'">
         </body>
         <script>
           function sendLinkCustom(){
