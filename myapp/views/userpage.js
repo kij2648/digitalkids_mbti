ae5d9e0fa3290131e7b2cb4d22e59a7335ebb0ad
김익준 <ikjun@gim-igjun-ui-MacBookAir.local>
2021-12-06 23:37:23 +0900

    슬래시 문제 해결

--- myapp/views/userpage.js
+++ myapp/views/userpage.js
@@ -18,12 +18,22 @@ module.exports = {
         <br>
 
         <h3> ${name}의 MBTI를 알려주세요. </h3>
-        <input type='button' value='MBTI 테스트 시작' onclick="location.href='./friend'">
-
+        <input type='button' value='MBTI 테스트 시작' onclick="urlRedirect()">
         <h3> 남들이 생각하는 자신의 MBTI가 궁금하다면? </h3>
         <input type='button' value='나의 Test 만들기' onclick="location.href='..'">
         
       </body>
+      <script>
+        var url33 = window.location.href
+        var lastChar = url33.slice(-1)
+        function urlRedirect(){
+          if(lastChar == "/"){
+            location.href=window.location.href+'friend'
+          } else {
+            location.href=window.location.href+'/friend'
+          }
+        }
+      </script>
       </html>
       `;
     }
